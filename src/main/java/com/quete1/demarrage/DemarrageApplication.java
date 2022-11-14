3e19b1812c5aa3520e972d631b963fc137890a26
FIRST_AOUCHICHE LAST_NAIM <naim.aouchiche@sncf.fr>
2022-11-14 11:26:43 +0100

    first commit

---
+++ src/main/java/com/quete1/demarrage/DemarrageApplication.java
@@ -0,0 +1,13 @@
+package com.quete1.demarrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemarrageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemarrageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quete1/demarrage/DemarrageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quete1.demarrage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemarrageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
